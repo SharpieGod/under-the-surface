2db3974f18b15fe639360532a966e6dc0970e614
SharpieGod <sabodarko09@gmail.com>
2026-02-28 15:06:25 -0800

    cool arms

--- arm_manager.gd
+++ arm_manager.gd
@@ -1,45 +1,44 @@
 extends Node2D
 
-@export var rest_length: float = 20.0
-@export var stiffness: float = 10.0
-@export var damping: float = 2.0
-@onready var L: Sprite2D = $"../Sprite2D"
+#@export var rest_length: float = 20.0
+#@export var stiffness: float = 10.0
+#@export var damping: float = 2.0
+@onready var L: Sprite2D = $"../Left Hand"
+@onready var R: Sprite2D = $"../Right Hand"
 
-var launched: bool = false
-var targetL: Vector2 = L.global_position
-
-@onready var player: CharacterBody2D = get_parent()
+@onready var player = $"../Camera2D"
 
 @onready var armL: Line2D = $Line2D
 @onready var armR: Line2D = $Line2D2
 
 func _process(delta: float) -> void:
-	
-	
-	if launched:
-		handle_grapple(delta)
+	handle_grapple(delta)
 
 func handle_grapple(delta: float) -> void:
-	var target_dir: Vector2 = player.global_position.direction_to(targetL)
-	var target_dist: float = player.global_position.distance_to(targetL)
+	#var target_dir_l: Vector2 = player.global_position.direction_to(L.global_position)
+	#var target_dist_l: float = player.global_position.distance_to(L.global_position)
+	#
+	#var target_dir_r: Vector2 = player.global_position.direction_to(R.global_position)
+	#var target_dist_r: float = player.global_position.distance_to(R.global_position)
 
-	var displacement: float = target_dist - rest_length
-	
-	var force: Vector2 = Vector2.ZERO
+	#var displacement: float = target_dist - rest_length
+	#
+	#var force: Vector2 = Vector2.ZERO
+#
+	#if displacement > 0:
+		#var spring_force_magnitude: float = stiffness * displacement
+		#var spring_force: Vector2 = target_dir * spring_force_magnitude
+		#
+		#var vel_dot: float = player.velocity.dot(target_dir)
+		#
+		#var damping_force: Vector2 = -damping * vel_dot * target_dir
+		#
+		#force = spring_force + damping_force
 
-	if displacement > 0:
-		var spring_force_magnitude: float = stiffness * displacement
-		var spring_force: Vector2 = target_dir * spring_force_magnitude
-		
-		var vel_dot: float = player.velocity.dot(target_dir)
-		
-		var damping_force: Vector2 = -damping * vel_dot * target_dir
-		
-		force = spring_force + damping_force
-
-	player.velocity += force * delta
+	#player.velocity += force * delta
 	
 	update_rope()
 
 func update_rope():
-	armL.set_point_position(1, armL.to_local(targetL))
+	armL.set_point_position(1, armL.to_local(L.global_position))
+	armR.set_point_position(1, armR.to_local(R.global_position))
--- receiver.gd
+++ receiver.gd
@@ -1,11 +1,12 @@
 extends CharacterBody2D
 
 @onready var tracker = $HandTracker
+@onready var camera = $Camera2D
 
 @export var cursor_left: Node2D
 @export var cursor_right: Node2D
-@export var max_speed: float = 30.0
-@export var smooth_factor: float = 0.75
+@export var max_speed: float = 100.0
+@export var smooth_factor: float = 0.90
 
 var _prev_closed: Dictionary = { true: false, false: false }
 var _smoothed_positions: Dictionary = {}
@@ -23,22 +24,31 @@ func _screen_to_world(position: Vector2) -> Vector2:
 	)
 	
 func _process(delta):
+	velocity += get_gravity() * delta * 2.4
+	
 	if _grab.size() > 0:
 		velocity = Vector2.ZERO
+	move_and_slide()
 	
 
 func _on_hand_updated(is_left: bool, position: Vector2, is_closed: bool):
 	var cursor = cursor_left if is_left else cursor_right
+	
 	if not cursor:
 		return
 
-	var world_pos = _screen_to_world(position)
+	var world_pos = _screen_to_world(position) * 1.5
 
 	var smoothed: Vector2
+	
 	if is_left in _smoothed_positions:
 		smoothed = _smoothed_positions[is_left].lerp(world_pos, smooth_factor)
 	else:
 		smoothed = world_pos
+		
+	if smoothed.length() > 600:
+		smoothed = smoothed.normalized() * 600
+		
 	_smoothed_positions[is_left] = smoothed
 
 	cursor.visible = true
